ba54bf72782ddd7aaae1c9594f62f6a2018a939c
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-04-11 09:17:32 +0100

    changes

--- person.rb
+++ person.rb
@@ -1,60 +1,32 @@
 class Person
-
   attr_accessor :name, :age
 
-
-
-  def initialize(age, name='')
-
+  def initialize(id, age, name = 'unknown', parent_permission: true)
+    @id = id
     @name = name
-
+    @parent_permission = parent_permission
     @age = age
-
   end
 
-
-
   def can_use_services?
-
     if is_of_age? || @parent_permission
-
-
 
       true
 
-
-
     else
-
-
 
       false
 
-
-
     end
-
   end
-
-
 
   def correct_name
-
     @name
-
   end
-
-
 
   private
 
-
-
   def of_age?
-
     @age >= 18
-
   end
-
 end
-
