d28ca1e5476c5cf84c01c4b0b1ec9e3bea292d75
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-04-11 08:52:45 +0100

    Final solution to decorator pattern

--- person.rb
+++ person.rb
@@ -1,120 +1,80 @@
 class Nameable
-
   attr_accessor :name
 
-
-
   def initialize(name)
-
     @name = name
-
   end
-
-
 
   def correct_name
-
     raise NotImplementedError, 'Subclass must implement correct_name method'
-
   end
-
 end
 
-
-
 class Person < Nameable
+  attr_accessor :name, :age
 
-  attr_accessor :name
-
-
-
-  def initialize(name)
-
+  def initialize(age, name)
     super(name)
 
     @name = name
-
+    @age = age
   end
 
+  def can_use_services?
+    if is_of_age? || @parent_permission
 
+      true
+
+    else
+
+      false
+
+    end
+  end
 
   def correct_name
-
     @name
-
   end
 
+  private
+
+  def of_age?
+    @age >= 18
+  end
 end
-
-
 
 class Decorator < Nameable
-
   def initialize(nameable)
-
     super(nameable.name)
 
-
-
     @nameable = nameable
-
   end
-
-
 
   def correct_name
-
     @nameable.correct_name
-
   end
-
 end
-
-
 
 class CapitalizeDecorator < Decorator
-
   def correct_name
-
     @nameable.correct_name.capitalize
-
   end
-
 end
-
-
 
 class TrimmerDecorator < Decorator
-
   def correct_name
-
     @nameable.correct_name.slice(0, 10)
-
   end
-
 end
 
-
-
-person = Person.new('maximilianus')
-
-
+person = Person.new(22, 'maximilianus')
 
 puts person.correct_name # "maximilianus"
 
-
-
 capitalized_person = CapitalizeDecorator.new(person)
-
-
 
 puts capitalized_person.correct_name # "Maximilianus"
 
-
-
 capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
 
-
-
 puts capitalized_trimmed_person.correct_name # "Maximilian"
-
