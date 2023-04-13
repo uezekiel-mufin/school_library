867205ea22c86e3f3a093c1432a96feb77032ba5
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-04-13 13:59:21 +0100

    Create method main in main.rb file

--- app.rb
+++ app.rb
@@ -1,19 +1,29 @@
 require_relative 'create_book'
+
 require_relative 'create_person'
+
 require_relative 'list_books'
+
 require_relative 'list_people'
+
 require_relative 'list_rental_by_id'
+
 require_relative 'create_rental'
+
 require_relative 'switch'
 
 puts 'Welcome to School Library'
+
 puts 'Please choose an option by entering a number:'
 
 class App
   def initialize(list_options)
     @books = []
+
     @people = []
+
     @rentals = []
+
     @list_options = list_options
   end
 
@@ -23,11 +33,17 @@ class App
     end
 
     choice = gets.chomp.to_i
+
     case choice
+
     when 1, 2, 3
+
       switch(choice)
+
     when 4, 5, 6, 7
+
       switch_rental(choice)
+
     end
   end
 end
--- main.rb
+++ main.rb
@@ -4,6 +4,9 @@ list_options = ['1 - List all books', '2 - List all people', '3 - Create a perso
 
                 '5 - Create a rental', '6 - List all rentals for a given person id', '7 - Exit']
 
-library = App.new(list_options)
+def main(list_options)
+  library = App.new(list_options)
 
-library.run
+  library.run
+end
+main(list_options)
