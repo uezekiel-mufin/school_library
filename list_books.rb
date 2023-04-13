33c0e08e449a792ffa04fd23e8e6e82b8f0ab3d2
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-04-13 20:06:23 +0100

    Implemented changes requested by reviewer

--- list_books.rb
+++ list_books.rb
@@ -1,11 +1,18 @@
 # Description: List all books in the library
 
+
+
 def list_books(books)
-  puts 'All books:'
 
   books.each_with_index do |book, _index|
-    puts "#{book.title.capitalize}, Author:#{book.author.capitalize}"
+
+    puts "Title: '#{book.title.capitalize}', Author: #{book.author.capitalize}"
+
   end
 
+
+
   puts 'No books in the library, create some.'
+
 end
+
